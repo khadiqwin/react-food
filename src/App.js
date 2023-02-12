cdde87e2742805358b592e7b3ba7948e880ee99a
khadijah <akinladekhadijah100@gmail.com>
2023-02-12 08:33:20 -0800

    Updated the food app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import CartProvider from "./Store/CartProvider";
+import Cart from "./Components/Cart/Cart";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,20 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import { useContext } from "react";
+import CartContext from "../../Store/CartContext";
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
+    </button>
+  );
+};
+export default HeaderCartButton;
